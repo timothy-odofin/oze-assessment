102f8f3ca15964c5f0f656764b837ad3148da68f
Odofin Oyejide <odofin@swipe.ng>
2022-09-14 10:44:41 +0100

    Implemented date validation

--- src/main/java/oze/career/assessment/model/dto/request/PatientRequest.java
+++ src/main/java/oze/career/assessment/model/dto/request/PatientRequest.java
@@ -5,15 +5,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import static oze.career.assessment.util.MessageUtil.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class PatientRequest {
+    @NotBlank(message = INVALID_FIRSTNAME)
     private String firstName;
+    @NotBlank(message = INVALID_LASTNAME)
     private String lastName;
     private String middleName;
     private LocalDate dob;
--- src/main/java/oze/career/assessment/util/MessageUtil.java
+++ src/main/java/oze/career/assessment/util/MessageUtil.java
@@ -6,7 +6,8 @@ public class MessageUtil {
     public final static String INTERNAL_SERVER_ERROR = "Unable to complete request at the moment.Please try again";
     public final static String SUCCESS = "success";
     public final static String FAILED = "fail";
-    public final static String DESCRIPTION_REQUIRED = "Invalid description, value is required";
+    public final static String INVALID_FIRSTNAME = "Invalid firstname, value is required";
+    public final static String INVALID_LASTNAME = "Invalid lastname, value is required";
 
     public final static String  UNAUTHORIZE = "You are not authorize to add/update item/category";
     public final static String  NO_TRANSACTION = "Transaction record not found for the selected %s";
